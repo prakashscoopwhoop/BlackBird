72bb7d25547f7cac2ac51f846430bcb6256145ff
Prakash singh <prakashsingh@Prakashs-MacBook-Pro.local>
2016-07-05 00:54:25 +0530

    dwonload images locally then saved data into db with images local path.

--- insert_interest.py
+++ insert_interest.py
@@ -1,11 +1,12 @@
 import csv,os
 from pymongo import MongoClient
+import urllib
 def read_file(file_to_analyze):
     lines = open(file_to_analyze,'r').readlines()
     to_write = []
     chk_to_write = True
     for l in lines:
-        if l==',,\n':
+        if l==',,\r\n':
             chk_to_write = False
             break
         elif chk_to_write:
@@ -13,7 +14,6 @@ def read_file(file_to_analyze):
         else:
             chk_to_write = False
             break   
-        
     open('temp.csv', 'w').writelines(to_write)
     records = []
     with open('temp.csv') as f:
@@ -21,12 +21,23 @@ def read_file(file_to_analyze):
         for row in f_csv:
             r_category = row['Category']
             r_sub_category = row['Name']
-            r_image = row['ImageUrl']
+            r_image = download_images_locally(row['ImageUrl'])
             records.append(
                 {'category': r_category, 'sub_category': r_sub_category, 'image': r_image})
     if os.path.isfile('temp.csv'):
         os.remove('temp.csv')
-    return records
+    return  records
+
+def download_images_locally(url):
+    cwd = os.getcwd()
+    if not os.path.exists('data'): 
+        os.makedirs('data')
+    filename = url.split('/')[-1]+".jpg"
+    os.chdir('data')
+    image_path = os.getcwd()
+    urllib.urlretrieve(url,filename)
+    os.chdir(cwd)
+    return image_path + filename
 
 if __name__== "__main__":
     client =  client = MongoClient()
@@ -36,4 +47,5 @@ if __name__== "__main__":
     for row in to_insert:
         col_name.save(row)
     print "insert successfully..."
+
     
