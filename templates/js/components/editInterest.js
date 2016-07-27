eed8dfe5234dee1f12773b417fb0925d05bee14c
ghazala1606 <ghazala@scoopwhoop.com>
2016-07-27 18:20:40 +0530

    #8: edit interest completed

--- templates/js/components/editInterest.js
+++ templates/js/components/editInterest.js
@@ -10,7 +10,11 @@ var EditInterestComponent = React.createClass({
 	    	data_arr: [],
 	    	imageValue: '',
 	    	interestValue: '',
-	    	idValue: ''
+	    	idValue: '',
+	    	optionValue: '',
+	    	optionValue: '',
+	    	cIdValue: '',
+	    	curIndVal: 0
 	    };
 	  },
 	
@@ -29,13 +33,23 @@ var EditInterestComponent = React.createClass({
 
 	},
 
-	interest_list_func : function(){
+	interest_list_func : function(c){
 
+		var that=this
 		$.get("http://0.0.0.0:8889/all_category", function (data){
 			
 			response =JSON.parse(data);
+			console.log(data)
 
 			for (var i = 0; i < response.data.length; i++) { 
+				
+				
+				if(response.data[i]._id==c){
+					console.log("matched")
+					that.setState({optionData: response.data[i].category});
+					that.setState({optionValue: response.data[i]._id});
+
+				}
 		
  				$(".select_").append('<option value="'+response.data[i]._id+'">'+response.data[i].category+'</option>')
  			}
@@ -43,36 +57,43 @@ var EditInterestComponent = React.createClass({
 
 	},
 
-	editInterest: function(id){
-
-		console.log(id)
-
-
-this.state.data_arr.map(function(item) {
-	
-	console.log(item)
-		if(item._id==id){
-
-			this.setState({idValue: item._id});
-
-			this.setState({imageValue: item.image});
-			
-			this.setState({interestValue: item.interest});
-
-			// this.setState({optionValue: item.interest});
-			
+	editInterest: function(id,ind){
+		var that = this;
+		
+		if(that.state.data_arr[ind]._id === id){
+			this.setState({
+				idValue: that.state.data_arr[ind]._id,
+				imageValue: that.state.data_arr[ind].image,
+				interestValue: that.state.data_arr[ind].interest,
+				curIndVal: ind
+			});	
+		}else{
+			console.log("id not matched");
+			this.state.data_arr.map(function(item) {					
+				if(item._id==id){
+					console.log(item)
+					console.log("edit matched");
+					console.log(item.interest);
+					this.setState({idValue: item._id});
+					this.setState({imageValue: item.image});				
+					this.setState({
+						interestValue: item.interest,
+						curIndVal:ind
+					});					
+				}
+			}.bind(this));
 		}
-}.bind(this));
-
-
+		
 		$('.edit_interest_popup').show();
 		return false;
 	},
 	
+
 	editPopupClose : function(){
 		$('.edit_interest_popup').hide();
 		return false;
 	},
+
 
 	handleChange: function(event,cc) {
 			// console.log(cc);
@@ -81,8 +102,9 @@ this.state.data_arr.map(function(item) {
 	    });
 	  },
 
-	editInterest_func: function(id_v){
 
+	editInterest_func: function(id_v){
+		var that=this
 		var editInterestData = {
 			_id: id_v,
 			image : $('#e_url').val(),
@@ -90,7 +112,8 @@ this.state.data_arr.map(function(item) {
 			keywords: [],
 			category_id: $('.select_').val()
 		};
-console.log(editInterestData)
+
+		console.log(editInterestData)
 		$.ajax({
                    type: "PUT",
                    url: "http://0.0.0.0:8889/edit_interest_data/",
@@ -99,17 +122,32 @@ console.log(editInterestData)
                    dataType: "json",
                      success: function(data){
                          console.log("success")
-                         console.log(data)
+                         
+                         var dataArrVal = that.state.data_arr;  
+                                              
+                         dataArrVal[that.state.curIndVal] = data.data;
+                         
+                         that.setState({
+                         	data_arr: dataArrVal
+                         });
+                         
+                         that.editPopupClose();
                      },
                      failure: function() {
-                         console.log("err");
+                         console.log("error");
                      }
 		        });
+    },
 
-	},
+  onClick: function(iId,cId,ind){
+  	
+      this.editInterest(iId,ind);
+      this.interest_list_func(cId);
+  },
 
 	render: function(){
         var component=this;
+        var that = this;
 		return(
 
 			<div>
@@ -132,7 +170,7 @@ console.log(editInterestData)
 									<div className="interest-select" onClick={this.interest_list_func} >
 										
 										<select className="select_">
-												<option value="null">-----select-----</option>
+												<option value={this.state.optionValue}>{this.state.optionData}</option>
 										</select>
 										
 									</div>
@@ -145,12 +183,14 @@ console.log(editInterestData)
 
 				<div className="all_interest_outerdiv">
 				{
+					
 					this.state.data_arr.map(function(item,i) {
+						// console.log(item)
           			return (
           				<div className="all_interest_div" id={item._id} key={i}>
           				   <img src={item.image}/>
           				   <div className="name">{item.interest}</div>
-          				   <img className="editIcon" src="edit-xxl.png" onClick={component.editInterest.bind(null,item._id)}/>
+          				   <img className="editIcon" src="edit-xxl.png" onClick={that.onClick.bind(null,item._id,item.category_id,i)}/>
           				</div>
           				)
       		 		})
